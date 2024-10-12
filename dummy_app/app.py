5eec1a6686cae0130cb6634e4decc388cacd411e
Tejas Prabhune <prabhune@berkeley.edu>
2024-10-12 15:00:45 -0700

    Add working transcription

--- dummy_app/app.py
+++ dummy_app/app.py
@@ -3,14 +3,16 @@ import websocket
 import json
 import base64
 
+import librosa
 import soundfile as sf
 import numpy as np
 
 example_wav, sr = sf.read("logo_top_left.wav")
+print(sr)
 
 ws = websocket.create_connection("ws://localhost:8765")
 
-example_wav = example_wav[:, 0]
+# example_wav = example_wav[:, 0]
 
 print("created websocket")
 
@@ -29,9 +31,18 @@ example_json_str = json.dumps(example_json)
 
 ws.send(example_json_str)
 
+logo_file = open("logo.jpg", "rb")
 
-result = ws.recv()
+logo_file_base64 = base64.b64encode(logo_file.read()).decode("utf-8")
 
-print(result)
+ws.send(json.dumps({
+    "event": "file",
+    "payload": logo_file_base64
+    }))
+
+ws.recv()
+ws.recv()
+
+# print(result)
 
 ws.close()
--- server/server.py
+++ server/server.py
@@ -1,32 +1,85 @@
+import io
+import openai
 import os
 import base64
 import json
 import sys
 import websockets
 import asyncio
+import soundfile as sf
 
 import websocket
 
 import numpy as np
+
+client = openai.OpenAI()
 
 oai_ws = websocket.create_connection("wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01", header=[
     "Authorization: Bearer " + os.environ["OPENAI_API_KEY"],
     "OpenAI-Beta: realtime=v1"
     ])
 
-print("Sending on oai websocket")
 oai_ws.send(json.dumps({
-    "type": "response.create",
-    "response": {
-        "modalities": ["text"],
-        "instructions": "Please assist the user.",
-        }
-    }));
+    "type": "session.update",
+    "session": {
+        "modalities": ["text", "audio"],
+        "instructions": "Your knowledge cutoff is 2023-10. You are a helpful assistant.",
+        "voice": "alloy",
+        "input_audio_format": "pcm16",
+        "output_audio_format": "pcm16",
+        "input_audio_transcription": {
+            "model": "whisper-1"
+        },
+        "turn_detection": {
+            "type": "server_vad",
+            "threshold": 0.5,
+            "prefix_padding_ms": 300,
+            "silence_duration_ms": 200
+        },
+        "tool_choice": "auto",
+        "temperature": 0.8
+    }
+}))
+
+print("Sending on oai websocket")
+
+def send_oai_audio(audio):
+    oai_ws.send(json.dumps({
+        "type": "conversation.item.create",
+        "item": {
+            "type": "message",
+            "role": "user",
+            "content": [
+                {
+                    "type": "input_audio",
+                    "audio": audio
+                    }
+                ]
+            }
+        }));
+    oai_ws.send(json.dumps({
+        "type": "response.create"
+        }))
 
 async def receive_oai():
+    await asyncio.to_thread(recv_oai_blocking)
+
+def recv_oai_blocking():
     while True:
         response = oai_ws.recv()
-        print(response)
+        response_json = json.loads(response)
+        print("response:", response_json["type"])
+
+        if response_json["type"] == "response.audio_transcript.delta":
+            print(response_json)
+
+def transcribe_audio(audio):
+    transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio,
+            response_format="text"
+            )
+    print(transcription)
 
 async def handle_audio(audio):
     print(audio.shape)
@@ -36,6 +89,16 @@ def parse_audio(payload):
     audio = np.frombuffer(base64.b64decode(audio_base64), dtype=np.float32)
     return audio
 
+def buffer_audio(audio):
+    sf.write("temp.wav", audio, 16000)
+    return open("temp.wav", "rb")
+
+def parse_file(payload):
+    file = payload.encode("utf-8")
+    file = base64.b64decode(file)
+
+    return file
+
 async def handle_event(websocket):
     async for message in websocket:
         
@@ -44,8 +107,15 @@ async def handle_event(websocket):
         print(message["event"])
 
         if message["event"] == "audio":
+            send_oai_audio(message["payload"])
             audio = parse_audio(message["payload"])
+            audio_file = buffer_audio(audio)
+            transcribe_audio(audio_file)
             await handle_audio(audio)
+        elif message["event"] == "file":
+            file = parse_file(message["payload"])
+            logo = open("logo.jpg", "wb")
+            logo.write(file)
 
         await websocket.send("received")
 
@@ -56,6 +126,7 @@ async def serve():
         await asyncio.Future()
 
 async def main():
+    print("Starting server")
     await asyncio.gather(
             serve()
             # receive_oai()
